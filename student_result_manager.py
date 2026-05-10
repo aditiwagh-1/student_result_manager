a86a0562bc4c7b396d0eca3c17efc5dadf15908b
aditiwagh-1 <waghaditi80@gmail.com>
2026-05-10 11:34:09 +0530

    adding the admin login system in in the project

--- student_result_manager.py
+++ student_result_manager.py
@@ -10,99 +10,111 @@ try:
 except FileNotFoundError:
     print("file not founded !!!")
     pass
+username = "Admin"
+password = "Admin@123"
 # while loop
 while True:
     # initialization of the project
-    print("----welcome to the student result management system----")
-    print("-------------------------------------------------------")
-    print("choose the option ......")
-    print("1. add student")
-    print("2. view all students")
-    print("3. check the result")
-    print("4. Result Calculation")
-    print("5. exit")
-    choice = int(input("enter your choices = "))
-    # if else conditions for adding the student in the empty dictionary
-    if choice == 1:
-        user_choice = input("you want to add student (yes/no) = ")
-        if (user_choice == "yes"):
-            stu_name = input("enter the student name = ")
-            if stu_name in student:
-                print("student already exits duffer!!")
-            else:
-                stu_marks = int(input("enter the student marks = "))
-            # adding the elments in he ditionary in thekey value pair
-                student[stu_name] = stu_marks
-                with open("students.txt", "a") as file:
-                    file.write(f"{stu_name} : {stu_marks}\n")
-                print(f"{stu_name} Successfully added !\n\n\n\n")
-        elif(user_choice == "no"):
-            print("exit !")
+        print("----welcome to the student result management system----")
+        print("-------------------------------------------------------")
+        print("\n\n")
+        
+        user_name = input("Enter the Name : ")
+        user_passwd = input("Enter the passwd : ")
+        if (username == user_name and password == user_passwd):
+            print("successfully Admin login")
             print("choose the option ......")
             print("1. add student")
             print("2. view all students")
             print("3. check the result")
-            print("4. exit")
-        else:
-            print("Invalide entry!!!!!!!!\n\n\n")
+            print("4. Result Calculation")
+            print("5. exit")
+            choice = int(input("enter your choices = "))
+            # if else conditions for adding the student in the empty dictionary
+            if choice == 1:
+                user_choice = input("you want to add student (yes/no) = ")
+                if (user_choice == "yes"):
+                    stu_name = input("enter the student name = ")
+                    if stu_name in student:
+                        print("student already exits duffer!!")
+                    else:
+                        stu_marks = int(input("enter the student marks = "))
+                       # adding the elments in he ditionary in thekey value pair
+                        student[stu_name] = stu_marks
+                        with open("students.txt", "a") as file:
+                            file.write(f"{stu_name} : {stu_marks}\n")
+                            print(f"{stu_name} Successfully added !\n\n\n\n")
+                elif(user_choice == "no"):
+                    print("choose the option ......")
+                    print("1. add student")
+                    print("2. view all students")
+                    print("3. check the result")
+                    print("4. exit")
+                else:
+                    print("Invalide entry!!!!!!!!\n\n\n")
     # this elif condition is for viewing the all students in the dictionary
-    elif choice == 2:
-        if not student:
-            print("no student found!!!")
-        else:
-            for stu_name, stu_marks in student.items():
-                    print(stu_name,":",stu_marks)
+            elif choice == 2:
+                if not student:
+                    print("no student found!!!")
+                else:
+                    for stu_name, stu_marks in student.items():
+                        print(stu_name,":",stu_marks)
      
     # this elif condition is for checking the result
-    elif choice == 3:
-        stu_name = input("enter the student name = ")
-        if stu_name in student:
-            stu_marks = student[stu_name]
-            print(stu_marks)
-            if stu_marks >= 35:
-                print("student passed !!")
-            else:
-                print("student failed !!")
-        else:
-            print("not found in the memory !!")
-    elif choice == 4: 
-        name = input("Enter the student name = ")
-        if name in student:
-            sub1 = int(input("enter the marks of subject 1 = "))
-            sub2 = int(input("enter the marks of subject 2 = "))
-            sub3 = int(input("enter the marks of subject 3 = "))
-            sub4 = int(input("enter the marks of subject 4 = "))
-            sub5 = int(input("enter the marks of subject 5 = "))
-            sub6 = int(input("enter the marks of subject 6 = "))
-            total = (sub1+sub2+sub3+sub4+sub5+sub6)
-            avg = total / 6
-            print("Total marks = ",total)
-            print("Total percentage = ",avg)
-            if(avg >=91 and avg<=100 ):
-                print("Grade A1\n")
-                print("Outstanding\n")
-            elif(avg >= 81 and avg <=90):
-                print("Grade A2\n")
-                print("Excellent\n")
-            elif(avg >=71 and avg <= 80):
-                print("Grade B1\n")
-                print("Very Good\n")
-            elif(avg >=61 and avg<= 70):
-                print("Grade B2\n")
-                print("Good\n")
-            elif(avg >=51 and avg<=60):
-                print("Grade C1\n")
-                print("Fair\n")
-            elif(avg >=33 and avg<= 50):
-                print("passed!\n")
-            elif(avg <=33):
-                print("Failed!\n")
-            else:
-                print("Invalide entry!\n")
-        else :
-            print("student not found !!")
+            elif choice == 3:
+                stu_name = input("enter the student name = ")
+                if stu_name in student:
+                    stu_marks = student[stu_name]
+                    print(stu_marks)
+                    if stu_marks >= 35:
+                        print("student passed !!")
+                    else:
+                        print("student failed !!")
+                else:
+                    print("not found in the memory !!")
+            elif choice == 4: 
+                name = input("Enter the student name = ")
+                if name in student:
+                    sub1 = int(input("enter the marks of subject 1 = "))
+                    sub2 = int(input("enter the marks of subject 2 = "))
+                    sub3 = int(input("enter the marks of subject 3 = "))
+                    sub4 = int(input("enter the marks of subject 4 = "))
+                    sub5 = int(input("enter the marks of subject 5 = "))
+                    sub6 = int(input("enter the marks of subject 6 = "))
+                    total = (sub1+sub2+sub3+sub4+sub5+sub6)
+                    avg = total / 6
+                    print("Total marks = ",total)
+                    print("Total percentage = ",avg)
+                    if(avg >=91 and avg<=100 ):
+                        print("Grade A1\n")
+                        print("Outstanding\n")
+                    elif(avg >= 81 and avg <=90):
+                        print("Grade A2\n")
+                        print("Excellent\n")
+                    elif(avg >=71 and avg <= 80):
+                        print("Grade B1\n")
+                        print("Very Good\n")
+                    elif(avg >=61 and avg<= 70):
+                        print("Grade B2\n")
+                        print("Good\n")
+                    elif(avg >=51 and avg<=60):
+                        print("Grade C1\n")
+                        print("Fair\n")
+                    elif(avg >=33 and avg<= 50):
+                        print("passed!\n")
+                    elif(avg <=33):
+                        print("Failed!\n")
+                    else:
+                        print("Invalide entry!\n")
+                else :
+                    print("student not found !!")
 # condition for the exiting from the program....
-    elif choice == 5:
-        print("Exiting .............\n")
-        break
-    
+            elif choice == 5:
+                print("Exiting .............\n")
+                break
+            else:
+                print("Invalide Entry!!")
+        else:
+            print("try again ")
+            continue
+ 
